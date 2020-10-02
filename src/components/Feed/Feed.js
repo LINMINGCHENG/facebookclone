71d8c3a616f0600bb85c225cbc95e3ae4f1cafa3
McJohnsonLin <48139258+LINMINGCHENG@users.noreply.github.com>
2020-10-02 04:23:09 -0400

    added

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import MessageSender from '../MessageSender/MessageSender'
 import StoryReel from '../Storyreel/StoryReel'
 import "./Feed.css"
 function Feed() {
     return (
         <div className="feed">
             <StoryReel/>
-            {/* MessageSender */}
+            <MessageSender/>
         </div>
     )
 }
--- src/components/MessageSender/MessageSender.js
+++ src/components/MessageSender/MessageSender.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import "./MessageSender.css"
+import Avatar from '@material-ui/core/Avatar'
 function MessageSender() {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+     }
+
+
+
     return (
-        <div className="messageSender">
-            
+      <div className="messageSender">
+        <div className="messageSender_top">
+                <Avatar />
+                <input className="messageSender_input"
+                 placeholder={`what's your mind`}></input>
+                <input placeholder="image URL(Optional)"></input>
+
+                <button onclick={handleSubmit} type="submit">
+                    Hidden submit
+                </button>
         </div>
-    )
+        <div className="messageSender_bottom">
+                
+                
+        </div>
+      </div>
+    );
 }
 
 export default MessageSender
