116bd3ee4aaaf7cd84b8b23c7fdc728152191f01
mingcheng lin <you@example.com>
2020-10-29 23:27:32 -0400

    push

--- src/components/Widgets/Widgets.js
+++ src/components/Widgets/Widgets.js
@@ -5,8 +5,8 @@ function Widgets() {
         <div className="widgets">
             <iframe
                 title="My Frame"
-                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=340&height=700&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-                width="340"
+                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=320&height=1400&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+                width="320"
                 height="100%"
                 style={{ border:"none",overflow:"hidden"}}
                 scrolling="no" frameborder="0"
